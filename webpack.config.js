5a90140dedfad8941e1b11b19d401fdf1b4c469b
Oskar Lesment <oskar-aleksander.lesment@tptlive.ee>
2024-02-01 14:21:45 +0200

    added webpack serve, works

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,13 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
+  devServer:{
+    static:{
+      directory:path.join(__dirname,'public'),
+    },
+    compress:true,
+    port:9000,
+  },
   module: {
     rules: [
         {
